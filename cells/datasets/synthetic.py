b47ce37d85b5029240978232756ece7fc3d65d53
Nina Miolane <nmiolane@gmail.com>
2022-06-13 15:47:00 -0700

    Add geodesics for square to rectangle

--- cells/datasets/synthetic.py
+++ cells/datasets/synthetic.py
@@ -1,6 +1,11 @@
 """Utils to load datasets of cells."""
 
 import geomstats.backend as gs
+import numpy as np
+from geomstats.geometry.discrete_curves import R2, DiscreteCurves
+
+CURVES_SPACE = DiscreteCurves(R2)
+METRIC = CURVES_SPACE.square_root_velocity_metric
 
 
 def rectangle(n_points_height, n_points_length, height, length):
@@ -64,19 +69,43 @@ def ellipse(n_points, a, b):
     return ellipse
 
 
-def unit_square(n_points_side):
+def square(n_points_side, side=1):
     """Return a square as an array of 2D points.
 
     This function returns a discrete closed curve representing a
     unit square as a array of its 2D points, in counter-clockwise order.
     """
-    return rectangle(n_points_side, n_points_side, 1, 1)
+    return rectangle(n_points_side, n_points_side, side, side)
 
 
-def unit_circle(n_points):
+def circle(n_points, radius=1):
     """Return a circle as an array of 2D points.
 
     This function returns a discrete closed curve representing the
     unit circle, as an array of 2D points.
     """
-    return ellipse(n_points, 1, 1)
+    return ellipse(n_points, 1 / radius, 1 / radius)
+
+
+def geodesics_square_to_rectangle(n_geodesics=10, n_times=20, n_points=40):
+    """Generate a dataset of geodesics that transform squares into rectangles."""
+    dim = 2
+    n_points_side = n_points_lengh = n_points_heigh = n_points // 4 + 1
+    sides = np.random.normal(loc=5, scale=0.2, size=(n_geodesics,))
+    heights = np.random.normal(loc=2, scale=0.1, size=(n_geodesics,))
+    lengths = np.random.normal(loc=10, scale=0.1, size=(n_geodesics,))
+
+    geodesics = gs.zeros((n_geodesics, n_times, n_points, dim))
+    times = gs.arange(0, 1, 1 / n_times)
+    for i_geodesic in range(n_geodesics):
+        start_square = square(n_points_side=n_points_side, side=sides[i_geodesic])
+        end_rect = rectangle(
+            n_points_height=n_points_heigh,
+            n_points_length=n_points_lengh,
+            height=heights[i_geodesic],
+            length=lengths[i_geodesic],
+        )
+        geodesic = METRIC.geodesic(initial_curve=start_square, end_curve=end_rect)
+        geodesics[i_geodesic] = geodesic(times)
+
+    return geodesics
--- tests/test_synthetic.py
+++ tests/test_synthetic.py
@@ -10,8 +10,8 @@ def test_rectangle():
 
 
 def test_square():
-    """Test that function unit_square runs and outputs correct shape."""
-    square = synthetic.unit_square(10)
+    """Test that function square runs and outputs correct shape."""
+    square = synthetic.square(10)
     assert square.shape == (36, 2), square.shape
 
 
@@ -22,6 +22,17 @@ def test_ellipse():
 
 
 def test_circle():
-    """Test that function unit_circle runs and outputs correct shape."""
-    circle = synthetic.unit_circle(20)
+    """Test that function circle runs and outputs correct shape."""
+    circle = synthetic.circle(20)
     assert circle.shape == (20, 2), circle.shape
+
+
+def test_geodesics_square_to_rectangle():
+    """Test that function geodesics_square_to_rectangle runs.
+
+    And outputs correct shape.
+    """
+    geodesics = synthetic.geodesics_square_to_rectangle(
+        n_geodesics=10, n_times=20, n_points=40
+    )
+    assert geodesics.shape == (10, 20, 40, 2), geodesics.shape
