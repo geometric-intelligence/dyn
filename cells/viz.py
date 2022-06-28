b533cc56c62c4314a0ce8d24be1ece769bd3b4e3
Nina Miolane <nmiolane@gmail.com>
2022-06-27 21:33:56 -0700

    Add better viz

---
+++ cells/viz.py
@@ -0,0 +1,19 @@
+"""Visualization tools."""
+
+
+import matplotlib
+
+
+def init_matplotlib():
+    """Configure style for matplotlib tutorial."""
+    fontsize = 18
+    matplotlib.rc("font", size=fontsize)
+    matplotlib.rc("text")
+    matplotlib.rc("legend", fontsize=fontsize)
+    matplotlib.rc("axes", titlesize=21, labelsize=14)
+    matplotlib.rc(
+        "font",
+        family="times",
+        serif=["Computer Modern Roman"],
+        monospace=["Computer Modern Typewriter"],
+    )
